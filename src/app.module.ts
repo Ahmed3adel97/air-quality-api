8919bf96a4cacb60308b6ae8bb2e7d6fe2891b77
Ahmed3adel97 <ahmedadelfathy97@gmail.com>
2024-09-01 08:34:18 +0300

    add swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AirQualityModule } from './air-quality/air-quality.module';
 import { ConfigModule } from '@nestjs/config';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { AirQualityService } from './air-quality/air-quality.service';
+import { ScheduleModule } from '@nestjs/schedule'; // Import ScheduleModule
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env', 
     }),
+    MongooseModule.forRoot(process.env.MONGODB_URI), // Connect to MongoDB here
+    ScheduleModule.forRoot(),
     AirQualityModule,
   ],
   controllers: [AppController],
